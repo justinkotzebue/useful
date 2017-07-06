65bb61e62a5dbc37f8ac02e40539396cb41bbc31
UsernameDiana <ms.strele@gmail.com>
2017-07-06 21:34:19 +0200

    read yaml dict

---
+++ make_dir.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+
+working_folder = '/home/jb/Downloads/test'
+folder_dict = dict(
+            f2 = os.path.join(d,'f2'),
+            f3 = os.path.join(d, 'f3'),
+            f4 = os.path.join(d,'f2', 'f21')
+            )
+
+
+def mkdir_using_dict(dict, working_folder):
+    """Creates directory structure given in dict in working_folder """
+    if not os.path.exists(working_folder):
+        os.mkdir(working_folder)
+    for a ,b in folder_dict.items():
+        try:
+            os.mkdir(b)
+        except:
+            continue
+
+
+def write_dict_to_yaml(file_path, dict_):
+    with open(file_path, 'w') as f:
+      yaml.dump(dict_, f, default_flow_style=False)
+
+
+def read_yaml_as_dict(yaml_path):
+    with open(yaml_path, 'r') as f:
+        ym=yaml.safe_load(f)
+    return ym
