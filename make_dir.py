dfde3780a0edb09422a86bfd7f5fd308acaab47a
UsernameDiana <ms.strele@gmail.com>
2017-07-08 14:58:17 +0200

    a

--- make_dir.py
+++ make_dir.py
@@ -21,11 +21,13 @@ def mkdir_using_dict(dict, working_folder):
 
 
 def write_dict_to_yaml(file_path, dict_):
+    '''write dict to yaml'''
     with open(file_path, 'w') as f:
       yaml.dump(dict_, f, default_flow_style=False)
 
 
 def read_yaml_as_dict(yaml_path):
+    '''read yaml and returns dict'''
     with open(yaml_path, 'r') as f:
         ym=yaml.safe_load(f)
     return ym
