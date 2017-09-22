323888b66827f8cbed48903e3d17671e62de29eb
justinkotzebue <juko@dhigroup.com>
2017-09-22 23:14:30 +0200

    csv

--- sonst/write_csv.py
+++ sonst/write_csv.py
@@ -4,4 +4,4 @@ with open('eggs.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar=';', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful shit'])
