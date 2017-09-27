954660e501104605a03705cf280ed96743e3320e
justinkotzebue <juko@dhigroup.com>
2017-09-27 20:47:55 +0200

    recl

--- raster/reclassify.py
+++ raster/reclassify.py
@@ -16,3 +16,13 @@ data[data > 10000] = np.nan
 data[data < 0] = np.nan
 with rasterio.open(outtif, mode='w', **meta) as dst:
     dst.write(data.astype(rasterio.float32),1)
+
+array = np.array([1,1,1,0,0,1,0,1,1,1,0,0])
+array = array.reshape(1,3,4)
+array.shape
+array
+a0 = np.zeros_like(array)ahd
+a0[:,2,2]=255
+array[a0]
+a0==255
+array[a0==255] = 3
